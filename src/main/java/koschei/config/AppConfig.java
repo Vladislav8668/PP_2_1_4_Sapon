0e545585ce19a45bdce63069e677d60bb5a6e851
Vladislav8668 <vlad866891@gmail.com>
2025-03-06 11:20:15 +0300

    Fixed #1

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -10,17 +10,17 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
-    public static Island2 getIsland(Wood3 wood) {
+    public Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
 
     @Bean
-    public static Egg6 getEgg(Needle7 neddle) {
+    public Egg6 getEgg(Needle7 neddle) {
         return new Egg6(neddle);
     }
 
     @Bean
-    public static Needle7 getNeedle(Deth8 deth) {
+    public Needle7 getNeedle(Deth8 deth) {
         return new Needle7(deth);
     }
 }
